1c8d4be239997a8043efd0714b213b949e757f63
Ota Aya <ota.a.ab@m.titech.ac.jp>
2018-06-22 12:26:17 +0900

    このファイルの変更に対するコメント

--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,36 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+    int a, b, i;
+    a = 0;
+    i = 0;
+    while(i < n){
+        b = A[i]/x;
+        a = a + b;
+        i = i+1;
+    }
+    return a > k-1;
+}
+
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    lb = 0;
+    ub = 10000;
+    while(ub - lb > 1){
+        int m = (lb + ub)/2;
+        if(p(m)){
+            lb = m;}
+        else {
+            ub = m;
+        }
+    }
+    printf("%d\n", lb);
+    
+    return 0;
 }
