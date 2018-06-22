ae4ec2c79affe24ab28c243357bcdae9bbc560e3
Ota Aya <ota.a.ab@m.titech.ac.jp>
2018-06-22 12:28:41 +0900

    このファイルの変更に対するコメント

--- src/works.c
+++ src/works.c
@@ -4,14 +4,48 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+    int i,j, l;
+    i = 0;
+    j = k;
+    l = x;
+    while(i < n && j >= 0){
+        if(l-A[i] >0){
+            l = l-A[i];
+            i = i+1;
+        }
+        if(l - A[i] < 0){
+            l = x;
+            j = j-1;
+        }
+        else {
+            l = x;
+            i = i + 1;
+            j = j -1;
+        }
+    }
+    return j >= 0;
+}
+
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    lb = 0;
+    ub = 10000;
+    int m;
+    while(ub - lb > 1){
+        m = (lb + ub)/2;
+        if(p(m)){
+            ub = m;
+        }
+        else {
+            lb = m;
+        }
+    }
+    printf("%d\n", ub);
+    return 0;
 }
