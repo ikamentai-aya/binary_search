157fac30247b55e2a99adecfb7f8d7ec0e4e1df1
Ota Aya <ota.a.ab@m.titech.ac.jp>
2018-06-22 12:19:48 +0900

    このファイルの変更に対するコメント

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,37 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+    int j, l, g;
+    j = 0;
+    l = 0;
+    while(j < n){
+        if(A[j]%x == 0){g = A[j]/x;}
+        else {g = A[j]/x + 1;}
+        l = l + g;
+        j = j + 1;
+    }
+    return l <= k;
+}
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    lb = 0;
+    ub = 10000;
+    int m;
+    while(ub-lb > 1){
+        m = (lb + ub)/2;
+        if(p(m)){
+            ub = m;
+        }
+        else {
+            lb = m;
+        }
+    }
+    printf("%d\n", ub);
+    return 0;
 }
